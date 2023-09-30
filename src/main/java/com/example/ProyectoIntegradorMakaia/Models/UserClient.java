2f39c7e13d4631645b9e0dca22a7b414127708eb
devJorgeGomez <jorgegomez98125@gmail.com>
2023-09-30 18:12:35 -0500

    Pequeños cambios

--- src/main/java/com/example/ProyectoIntegradorMakaia/Models/UserClient.java
+++ src/main/java/com/example/ProyectoIntegradorMakaia/Models/UserClient.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.util.List;
+import java.util.Set;
 
 @Data
 @Entity(name = "users")
@@ -18,11 +19,12 @@ public class UserClient {
 
     public UserClient() {}
 
-    public UserClient(String username, String password, String email, List<Authority> authorities) {
+    public UserClient(String username, String password, String email, List<Authority> authorities, Set<Roles> roles) {
         this.username = username;
         this.email = email;
         this.password = password;
         this.authorities = authorities;
+        this.roles=roles;
     }
 
     @ManyToMany(fetch = FetchType.EAGER)
@@ -30,5 +32,11 @@ public class UserClient {
             joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "authority_id", referencedColumnName = "id"))
     List<Authority> authorities;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "roles_id", referencedColumnName = "id"))
+    Set<Roles> roles;
 
 }
