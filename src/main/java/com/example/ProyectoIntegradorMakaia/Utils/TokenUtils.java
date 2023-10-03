676d8983720826527f3d6bc5231b5a36ed1352e9
danessti <danessti@gmail.com>
2023-10-02 20:35:32 -0500

    se hizo un cambio con una posible funcion obsoleta con TokenUtils

--- src/main/java/com/example/ProyectoIntegradorMakaia/Utils/TokenUtils.java
+++ src/main/java/com/example/ProyectoIntegradorMakaia/Utils/TokenUtils.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import java.util.Collections;
@@ -16,22 +17,22 @@ public class TokenUtils {
     private final static String ACCESS_TOKEN_SECRET = "4qhq8LrEBfYcaRHxhdb9zURb2rf8e7Ud";
     private final static Long ACCESS_TOKEN_VALIDITY_SECONDS =2_592_000L;
 
-    public static String createToken (String nombre, String email){
+    public static String createToken (String nombre, String email) {
         long expirationTime = ACCESS_TOKEN_VALIDITY_SECONDS * 1000;
-        Date expirationDate = new Date(System.currentTimeMillis()+expirationTime);
+        Date expirationDate = new Date(System.currentTimeMillis() + expirationTime);
 
-        Map<String,Object> extra = new HashMap<>();
+        Map<String, Object> extra = new HashMap<>();
         extra.put("nombre", nombre);
 
         return Jwts.builder()
                 .setSubject(email)
                 .setExpiration(expirationDate)
                 .addClaims(extra)
-                .signWith(SignatureAlgorithm.HS256, ACCESS_TOKEN_SECRET)
+                .signWith(Keys.hmacShaKeyFor(ACCESS_TOKEN_SECRET.getBytes()), SignatureAlgorithm.HS384)
                 .compact();
     }
 
-    public  static UsernamePasswordAuthenticationToken getAuthentication(String token){
+    public static UsernamePasswordAuthenticationToken getAuthentication(String token) {
         try {
             Claims claims=Jwts.parser()
                     .setSigningKey(ACCESS_TOKEN_SECRET.getBytes())
@@ -41,7 +42,7 @@ public class TokenUtils {
             String email = claims.getSubject();
 
             return new UsernamePasswordAuthenticationToken(email, null, Collections.emptyList());
-        }catch (JwtException e) {
+        } catch (JwtException e) {
             return null;
         }
     }
